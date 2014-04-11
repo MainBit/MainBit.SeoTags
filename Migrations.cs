0640c2b0c73a08ae76810d846eb079fd8597d200
neTp9c <alexpetryakov@gmail.com>
2014-04-11 15:32:04 +0400

    Add migration.

---
+++ Migrations.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace MainBit.SeoTags {
+    public class Migrations : DataMigrationImpl {
+
+        public int Create() {
+            ContentDefinitionManager.AlterPartDefinition("SeoTagsPart", builder => builder
+                .Attachable()
+                .WithDescription("Title, Keywords, Description, Canonical, Pagging.")
+            );
+
+            return 1;
+        }
+    }
+}
